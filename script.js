430e9280d271ba175b1eba55b1d1e31fe4b2e89d
Gustavo Teixeira <teixeiragoesgustavo@gmail.com>
2024-08-31 10:47:43 -0300

    script.js

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
-let metro = parseInt(prompt('Digite o valor em Metros'))
-const opção = parseInt(prompt(`Escolha uma das opções abaixo para converter \n
-1 - milímetro (mm)\n 2 - centímetro (cm)\n 3 - decímetro (dm)\n4 - decâmetro (dam)\n 5 - hectômetro (hm)\n 6 - quilômetro (km)`))
+let metro = 10
+const opção = 3
 switch (opção) {
     case 1:
-        alert(`${metro}m equivale à ` + (metro *= 1000 ) + 'mm')
+        console.log(`${metro}m equivale à ` + (metro *= 1000 ) + 'mm')
         break
     case 2:
-        alert(`${metro}m equivale à ` + (metro *= 100) + 'cm')
+        console.log(`${metro}m equivale à ` + (metro *= 100) + 'cm')
         break
     case 3:
-        alert(`${metro}m equivale à ` + (metro *= 10) + 'dm') 
+        console.log(`${metro}m equivale à ` + (metro *= 10) + 'dm') 
         break
     case 4: 
-        alert(`${metro}m equivale à ` + (metro /= 10) + 'dam')
+        console.log(`${metro}m equivale à ` + (metro /= 10) + 'dam')
         break
     case 5:
-        alert(`${metro}m equivale à ` + (metro /= 100) + 'hm') 
+        console.log(`${metro}m equivale à ` + (metro /= 100) + 'hm') 
         break
     case 6:
-        alert(`${metro}m equivale à ` + (metro /= 1000) + 'km')
+        console.log(`${metro}m equivale à ` + (metro /= 1000) + 'km')
         break
     default:
-        alert('Opção inválida!')                
+        console.log('Opção inválida!')                
 }
